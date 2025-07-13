d26ca6526c7103ddd32fa81bf1cb1e9a2c3c8b32
Pratyush Kumar <pratyush.xdev@gmail.com>
2025-07-13 10:09:24 +0530

    fix font

--- src/app/chat/layout.js
+++ src/app/chat/layout.js
@@ -1,10 +1,9 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import { Inter, Poppins, Ubuntu, Noto_Sans } from "next/font/google";
-import "./globals.scss";
 import Sidebar from "@/components/Sidebar.jsx";
 import TopBar from "@/components/ui/TopBar";
-import Head from "next/head";
+import "./globals.scss";
 
 const inter = Inter({
   subsets: ["latin"],
