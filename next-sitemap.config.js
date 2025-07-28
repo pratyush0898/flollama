d0fa540d4fe3ff5aa2d7755cdff8284f0ad87b79
Pratyush Kumar <pratyush.xdev@gmail.com>
2025-07-28 21:45:32 +0530

    Add next/sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,23 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://flollama.in",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: "weekly",
+  priority: 0.7,
+  exclude: ["/chat"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: '*',
+        disallow: '/chat/',
+      },
+    ],
+    additionalSitemaps: ["https://flollama.in/server-sitemap.xml"],
+  },
+};
